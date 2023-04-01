e1efa19a2d6bef119f4bde535d9b0fc8a5fd98b8
Zvezdan <zvezdanjancev@hotmail.com>
2023-04-01 02:51:54 +0200

    Fix method sumEvenNumbers

--- SILab1.java
+++ SILab1.java
@@ -6,7 +6,7 @@ public class SILab1 {
     public static List<Integer> filterOddNumbers(List<Integer> list) {
         List<Integer> result = new ArrayList<>();
         for (Integer el : list) {
-            if (el % 2 == 0) {
+            if (el % 2 != 0) {
                 result.add(el);
             }
         }
@@ -14,7 +14,7 @@ public class SILab1 {
     }
 
     private static int sumEvenNumbers(List<Integer> list) {
-        int sum = 1;
+        int sum = 0;
         for (Integer el : list) {
             if (el % 2 == 0) {
                 sum *= el;
@@ -30,7 +30,7 @@ public class SILab1 {
 
         List<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             list.add(sc.nextInt());
         }
 
